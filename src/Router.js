34a70c695f026464324ff14c79f797df9b34ed99
seonday <gptjs0613@gmail.com>
2022-12-05 16:22:01 +0900

    Add: first commit 초기 세팅 완료

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Main from './pages/Main/Main';
+import ProductList from './pages/ProductList/ProductList';
+import Product from './pages/ProductList/Product/Product';
+import Footer from './components/Footer/Footer';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/productList" element={<ProductList />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default Router;
