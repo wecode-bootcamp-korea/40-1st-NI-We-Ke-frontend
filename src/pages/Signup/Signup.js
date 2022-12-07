4d0b5ed6fd1c49b7445668318071018a8ad85c8e
seonday <gptjs0613@gmail.com>
2022-12-07 14:48:28 +0900

    Modify: 로그인 기능 구현중

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,17 +1,13 @@
 import React from 'react';
-// import { useLocation } from 'react-router-dom';
 import User from '../Signup/User/User';
 import './Signup.scss';
 
 const Signup = () => {
-  // const location = useLocation();
-  // const currentURL = location.path;
   const isSelectLogin = true;
-  // currentURL === '/login' && true;
 
   return (
     <section className="signup">
-      <User text={isSelectLogin ? LOGIN_TEXT : SIGNUP_TEXT} />
+      <User text={isSelectLogin ? SIGNUP_TEXT : LOGIN_TEXT} />
     </section>
   );
 };
@@ -27,8 +23,8 @@ const LOGIN_TEXT = {
 };
 
 const SIGNUP_TEXT = {
-  title: '계속',
-  link: '가입을 위해 이메일을 입력해주세요',
+  title: '가입',
+  link: '가입을 위해 이름을 입력해주세요',
   url: '/login',
   agree:
     '계속 진행하면 나위키의 개인 정보 처리 방침 및 이용약관에 동의하게 됩니다.',
--- src/pages/Signup/User/User.js
+++ src/pages/Signup/User/User.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './User.scss';
 
 const User = ({ text }) => {
   const { title, link, url, agree } = text;
+  const [userForm, setUserForm] = useState('');
+
+  function onChangeUserInfo(e) {
+    const {name, value}
+  }
 
   return (
     <section className="user">
       <p>{link}</p>
       <div className="userForm">
-        <input className="firstInput" type="email" placeholder="이메일" />
-        {title === '로그인' && <input type="password" placeholder="비밀번호" />}
+        {title === '가입' && (
+          <input className="firstInput" type="name" 
+          onChange={} placeholder="이름" />
+        )}
+        <input className="firstInput" type="email" 
+        onChange={}
+        placeholder="이메일" />
+        <input type="password"  onChange={}
+        placeholder="비밀번호" />
         <p className="agree">{agree}</p>
         <Link to={url} className="link">
           <button>{title}</button>
