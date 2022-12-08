80d2d2c22dea5630bff316db59be82619e593d94
metamro <en160305@gmail.com>
2022-12-08 15:52:00 +0900

    Modify:Cart 레이아웃 추가하는 중

--- src/pages/Cart/CartList.js
+++ src/pages/Cart/CartList.js
@@ -1,5 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-useEffect(() => {
-  fetch('./data/Cartdata.json');
-});
+const CartList = () => {
+  return(
+    <div>
+    <img src={} alt="productphoto"/>  
+    <p>제품 이름</p>
+    <p>제품 종류</p>
+    <p>제품 색상</p>
+    <label for='size'>사이즈</label> <imput type='button' id='size'>250</imput>
+    </div>
+  )
+}
