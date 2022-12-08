43739b95435faf89e6392a0769e4d4d5e99a04d5
OGU <ogu@OGUui-MacBookAir.local>
2022-12-08 21:09:40 +0900

    Modify : 검색창 수정중

--- src/components/Nav/Search/Search.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const search = () => {
-  return <input className="search" type="text" />;
-};
-
-export default search;
---
+++ src/components/Nav/Sideoption/Search/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Search.scss';
+
+const search = () => {
+  return (
+    <div className="searchSection">
+      <input className="search" type="text" />
+
+      <section className="dropdownSection">
+        <div className="searchHeader"></div>
+        <section className="searchResult">h11i</section>
+      </section>
+    </div>
+  );
+};
+
+export default search;
