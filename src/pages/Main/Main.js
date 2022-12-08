9e7652c93616964c62ba6f57ad1d6c019106a392
OGU <ogu@OGUui-MacBookAir.local>
2022-12-08 15:59:17 +0900

    ADD: main layout 추가

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Content from './Content/Content';
+import './Main.scss';
+import MAIN_DATA from './maindata';
 
 const Main = () => {
-  return <h1>Main</h1>;
+  return (
+    <div>
+      {MAIN_DATA.map(data => {
+        return (
+          <Content
+            key={data.id}
+            img={data.img}
+            title={data.title}
+            text={data.text}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Main;
