ed633f8a021c65e0249983bceefec9e651c03fc5
OGU <ogu@OGUui-MacBookAir.local>
2022-12-09 21:41:58 +0900

    Modify : draw modal 추가

--- src/pages/DrawPage/Drawpage.js
+++ src/pages/DrawPage/Drawpage.js
@@ -1,38 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Drawpage.scss';
-import Drawmodal from './Drawmodal/Drawmodal';
 
 const Drawpage = () => {
+  const [isVisible, setIsVisble] = useState(false);
+
+  const onClickHandler = () => {
+    setIsVisble(true);
+  };
+
   return (
     <div className="drawPage">
       <article className="container">
+        {/* TODO : imgSection img width는 퍼센트 혹은 반응형으로 만들자 */}
         <section className="imgSection"></section>
         <section className="stickySection">
           <section className="productExplain">
             1<br />
             1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            1<br />
-            <button>응모하기</button>
+            <button onClick={onClickHandler}>응모하기</button>
           </section>
         </section>
       </article>
       <div className="dummy"></div>
-      <Drawmodal />
+      {isVisible && <article className="drawModal">모달입니다</article>}
     </div>
   );
 };
