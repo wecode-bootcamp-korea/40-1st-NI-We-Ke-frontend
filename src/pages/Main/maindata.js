1980218c1363eb7115bf20b8889b9585fde5366a
OGU <ogu@OGUui-MacBookAir.local>
2022-12-13 18:15:10 +0900

    WIP

--- src/pages/Main/maindata.js
+++ src/pages/Main/maindata.js
@@ -1,7 +1,22 @@
 const MAIN_DATA = [
-  { id: 1, img: '/images/soccer.jpg', title: '축구', text: '재밋다!' },
-  { id: 2, img: '/images/basketball.jpg', title: '농구', text: '재밋다!' },
-  { id: 3, img: '/images/baseball.jpg', title: '야구', text: '재밋다!' },
+  {
+    id: 1,
+    img: '/images/soccer.jpg',
+    title: 'Guus Hiddink',
+    text: '실력이 떨어지면 남들보다 더 많은 노력을 기울여 이를 극복하면 되는 것이다. 무엇보다 가장 필요한 것은 스스로 하고자 하는 의지이다',
+  },
+  {
+    id: 2,
+    img: '/images/basketball.jpg',
+    title: 'Michael Jeffrey Jordan',
+    text: '나는 실패는 받아들일 수 있다. 누구나 실패는 하기 때문이다. 하지만 시도하지 않는 것은 받아들일 수 없다',
+  },
+  {
+    id: 3,
+    img: '/images/baseball.jpg',
+    title: 'Usain Bolt',
+    text: '나보다 시작이 나은 선수들이 있겠지만, 나는 끝이 강한 선수이다',
+  },
 ];
 
 export default MAIN_DATA;
