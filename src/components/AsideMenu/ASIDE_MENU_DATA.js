7e398675e80079a4b7e096526be9a6e9a753393e
seonday <gptjs0613@gmail.com>
2022-12-13 14:35:57 +0900

    Modify: 레이아웃 정리 완료

--- src/components/AsideMenu/ASIDE_MENU_DATA.js
+++ src/components/AsideMenu/ASIDE_MENU_DATA.js
@@ -3,23 +3,13 @@ export const ASIDE_MENU_DATA = [
     id: 1,
     text: '색상',
     detail: [
-      { color: 'black', text: '블랙' },
+      { color: 'red', text: '빨강' },
       { color: 'blue', text: '블루' },
-      { color: 'brown', text: '브라운' },
-      { color: 'green', text: '그린' },
+      { color: 'orange', text: '오렌지' },
     ],
   },
   {
     id: 2,
-    text: '신발높이',
-    detail: [
-      { id: '29', text: '로우 탑' },
-      { id: '30', text: '미들 탑' },
-      { id: '31', text: '하이 탑' },
-    ],
-  },
-  {
-    id: 3,
     text: '너비',
     detail: [
       { id: '9', text: '좁은 편' },
@@ -28,26 +18,24 @@ export const ASIDE_MENU_DATA = [
     ],
   },
   {
-    id: 4,
+    id: 3,
     text: '용도',
     detail: [
-      { id: '13', text: '습한 날씨' },
-      { id: '14', text: '추운 날씨' },
+      { id: '12', text: '운동 / 스포츠' },
+      { id: '13', text: '라이프 스타일' },
     ],
   },
   {
-    id: 5,
+    id: 4,
     text: '소재',
-    detail: [{ id: '17', text: '지속 가능 컬렉션' }],
+    detail: [{ id: '14', text: '지속 가능 컬렉션' }],
   },
   {
     id: 6,
     text: '특징',
     detail: [
-      { id: '21', text: '고스트 레이스 시스템' },
-      { id: '22', text: '스파이크 없음' },
-      { id: '23', text: '내수성' },
-      { id: '24', text: '방수' },
+      { id: '15', text: '내수성' },
+      { id: '16', text: '방수' },
     ],
   },
   {
